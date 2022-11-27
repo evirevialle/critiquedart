1acbd6f3870be9706a7c0c164b7a51b42bbce819
Evan Virevialle <83653214+evirevialle@users.noreply.github.com>
2022-11-27 12:10:35 +0100

    Update csv_encode_auteurs.php

--- templates/API/csv_encode_auteurs.php
+++ templates/API/csv_encode_auteurs.php
@@ -33,7 +33,7 @@
 $sql = "SELECT `pk_id_critiqueDart` as id, `prenom`, `nom`, `anneeNaissance` as naissance, `anneeMort` as mort, ISNI FROM `critiquedart` WHERE 1 ORDER BY nom";
 	//echo $sql;
 	//$db_conn=mysql_connect("localhost", "root", "root");
-	$db_conn=mysqli_connect("localhost", "labexcap","MEDI@98313", "critiquesdart");
+	$db_conn=mysqli_connect("localhost", "","", "critiquesdart");
 	mysqli_set_charset($db_conn, 'utf8');
 	mysqli_select_db($db_conn, "critiquesdart") or die('Problème de connexion à la base.');
     // output as an attachment
